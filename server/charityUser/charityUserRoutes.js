5a8fda7dd52ddc43fccde97e212c0e9b605ed4c6
Nikhil Ballaney <nballaney@gmail.com>
2014-12-11 16:44:03 -0800

    (feat) Tokens and authentication

--- server/charityUser/charityUserRoutes.js
+++ server/charityUser/charityUserRoutes.js
@@ -2,5 +2,6 @@ var charityUserController = require('./charityUserController');
 
 module.exports = function(app) {
   app.post('/signup', charityUserController.signup);
-  app.post('/profile', charityUserController.profile);
+  app.post('/profile', checkToken(), charityUserController.profile);
+  app.post('/login', charityUserController.login);
 }
