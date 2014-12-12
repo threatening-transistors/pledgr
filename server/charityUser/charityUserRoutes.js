7386a53ed4559d82c9a17de769ab34f95fd3fd7b
Nikhil Ballaney <nballaney@gmail.com>
2014-12-11 19:14:52 -0800

    (feat) Add logout route

--- server/charityUser/charityUserRoutes.js
+++ server/charityUser/charityUserRoutes.js
@@ -5,6 +5,6 @@ module.exports = function(app) {
   app.post('/signup', charityUserController.signup);
   app.post('/profile', helpers.checkToken, charityUserController.profile);
   app.post('/login', charityUserController.login);
-
+  app.post('/logout', charityUserController.logout);
   app.get('/', charityUserController.listAll);
 }
