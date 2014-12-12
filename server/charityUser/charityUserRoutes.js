2bb8f7d341aeafbc88acdb8a0e628a12c3c3d5fe
Nikhil Ballaney <nballaney@gmail.com>
2014-12-11 19:22:09 -0800

    (fix)

--- server/charityUser/charityUserRoutes.js
+++ server/charityUser/charityUserRoutes.js
@@ -3,8 +3,8 @@ var helpers = require('../config/helpers');
 
 module.exports = function(app) {
   app.post('/signup', charityUserController.signup);
-  app.post('/profile', helpers.checkToken, charityUserController.profile);
+  app.get('/profile', helpers.checkToken, charityUserController.profile);
   app.post('/login', charityUserController.login);
-  app.post('/logout', charityUserController.logout);
+  app.get('/logout', charityUserController.logout);
   app.get('/', charityUserController.listAll);
 }
