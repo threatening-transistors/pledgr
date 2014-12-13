fc6fd0f6670c5e9db61058fb70151f251f8a862a
Derek Barncard <derek@barncard.com>
2014-12-12 17:07:38 -0800

    semi-working two-step donation transfers implemented

--- client/app/charityList/charityList.js
+++ client/app/charityList/charityList.js
@@ -15,13 +15,35 @@ angular.module('pledgr.charityList', [])
 		$scope.paymentBankAccount = $scope.charities[index].bank_account;
 		$scope.paymentRecipient = $scope.charities[index].recipient_id;
 		$scope.paymentCharity = $scope.charities[index].name;
+		$scope.paymentCharityId = $scope.charities[index]._id;
 		$('.pay-modal').modal();
 	};
 
 	$scope.processDonation = function(){
+		var $form = $('#cc-form');
 		$('.donate-button').text("Donation in progress!").toggleClass('disabled');
 		$scope.donationInProgress = true;
+
+		$scope.stripeInit();
 		
+	    Stripe.card.createToken($form, function(status, response){
+	    	//console.log(response);
+	    	//create transfer
+	    	CharityListFactory.makeDonationTransfer({
+	    		donor_id: '78agdfouyagd', //herd-curded
+	    		charity_id: $scope.paymentCharityId,
+	    		amount: 200, //let's make this plz
+	    		description: 'Yay, I made a donation!'
+	    	}).then(function(data){
+	    		console.log('got it', data);
+	    	});
+	    });
+;
+
+	};
+
+	$scope.stripeInit = function(){
+		Stripe.setPublishableKey('pk_test_7qhH8GqrwSZvzFQVSWmiMiu1');
 	};
 
 	$scope.getCharityList();
@@ -33,14 +55,26 @@ angular.module('pledgr.charityList', [])
 
 	var getCharityList = function(){
 		return $http({
-			http:'GET',
+			method:'GET',
 			url:'/api/charityUser/'
 		}).then(function(res){
 			return res.data;
 		});
 	};
 
+	var makeDonationTransfer = function(xferInfo){
+		return $http({
+			method:'POST',
+			url: '/api/donation/send',
+			data: xferInfo
+		}).then(function(res){
+			console.log("makeDonationTransfer", res.data);
+			return res.data;
+		});
+	}
+
 	return {
-		getCharityList: getCharityList
+		getCharityList: getCharityList,
+		makeDonationTransfer: makeDonationTransfer
 	};
 });
